9193ecee6385f4566ea45ea0048eb4998a9ea333
Roberto Friber <75671954+FriberNamzug@users.noreply.github.com>
2022-11-30 12:18:57 -0600

    Se instalan las dependencias y configura el entorno

---
+++ client/tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      color1: "#273C2C",
+      color2: "#626868",
+      color3: "#939196",
+      color4: "#D3C1D2",
+      color5: "#FFE2FE"
+    },
+  },
+  plugins: [],
+}
